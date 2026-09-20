e1183c6194b25789495e11f62a95e7cd2b576544
Hamza Elshafie <74862538+HamzaElshafie@users.noreply.github.com>
2026-09-20 19:27:02 +0800

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.py
@@ -0,0 +1,27 @@
+import heapq
+import math
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        origin = [0,0]
+        min_heap = []
+        res = []
+
+        for point in points:
+            # calculate distance to origin
+            distance = euclidean_distance(point, origin)
+            heapq.heappush(min_heap, (distance, point))
+
+        for _ in range(k):
+            res.append(heapq.heappop(min_heap)[1])
+
+        return res
+    
+def euclidean_distance(x1, x2):
+    return math.sqrt((x1[0] - x2[0])**2 + (x1[1]-x2[1])**2)
+
+
+# Initial approach
+# So we want to iterate over each element in `points`, calculate its distance to the origin, add it to a "min heap"
+
+# loop k times popping from min heap, returning the val last popped once we exit loop
